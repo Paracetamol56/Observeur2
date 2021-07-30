10563a1678c342cc4a7c851407ec0357bc1fbfc2
Mathéo <matheo.galu56@gmail.com>
2021-07-30 23:10:37 +0200

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,6 @@
+#include "Error in " Util.relativeFilePath('D:/ASTRONOMIE/Classeur d'astronomie/Catalogue/Observeur2/Observer2/mainwindow.h', 'D:/ASTRONOMIE/Classeur d'astronomie/Catalogue/Observeur2/Observer2' + '/' + Util.path('mainwindow.cpp'))": SyntaxError: Expected token `)'"
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
+{
+
+}
