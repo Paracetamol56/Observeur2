5fee026ce15cf6b84d0e092ab2c733618c445e07
Mathéo <matheo.galu56@gmail.com>
2021-07-31 10:37:00 +0200

    Added constellwindow class

---
+++ constelwindow.cpp
@@ -0,0 +1,6 @@
+#include "Error in " Util.relativeFilePath('D:/ASTRONOMIE/Classeur d'astronomie/Catalogue/Observeur2/Observer2/constelwindow.h', 'D:/ASTRONOMIE/Classeur d'astronomie/Catalogue/Observeur2/Observer2' + '/' + Util.path('constelwindow.cpp'))": SyntaxError: Expected token `)'"
+
+ConstelWindow::ConstelWindow()
+{
+
+}
---
+++ constelwindow.h
@@ -0,0 +1,11 @@
+#ifndef CONSTELWINDOW_H
+#define CONSTELWINDOW_H
+
+
+class ConstelWindow
+{
+public:
+    ConstelWindow();
+};
+
+#endif // CONSTELWINDOW_H
