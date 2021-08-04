eebec296afe886c03cba0bad04f0b40d611f7844
Mathéo <matheo.galu56@gmail.com>
2021-08-04 16:46:19 +0200

    New class astronomicalcoordinatesystems

---
+++ astronomicalcoordinatesystems.cpp
@@ -0,0 +1,6 @@
+#include "Error in " Util.relativeFilePath('D:/ASTRONOMIE/Classeur d'astronomie/Catalogue/Observeur2/Observer2/astronomicalcoordinatesystems.h', 'D:/ASTRONOMIE/Classeur d'astronomie/Catalogue/Observeur2/Observer2' + '/' + Util.path('astronomicalcoordinatesystems.cpp'))": SyntaxError: Expected token `)'"
+
+EquatorialPostition::EquatorialPostition()
+{
+
+}
---
+++ astronomicalcoordinatesystems.h
@@ -0,0 +1,11 @@
+#ifndef EQUATORIALPOSTITION_H
+#define EQUATORIALPOSTITION_H
+
+
+class EquatorialPostition
+{
+public:
+    EquatorialPostition();
+};
+
+#endif // EQUATORIALPOSTITION_H
