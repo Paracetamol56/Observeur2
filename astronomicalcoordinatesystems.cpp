f1b79ea6cbadc141af10d189059dfbef73617437
Mathéo <matheo.galu56@gmail.com>
2021-08-04 17:22:11 +0200

    Save

--- astronomicalcoordinatesystems.cpp
+++
@@ -1,6 +0,0 @@
-#include "Error in " Util.relativeFilePath('D:/ASTRONOMIE/Classeur d'astronomie/Catalogue/Observeur2/Observer2/astronomicalcoordinatesystems.h', 'D:/ASTRONOMIE/Classeur d'astronomie/Catalogue/Observeur2/Observer2' + '/' + Util.path('astronomicalcoordinatesystems.cpp'))": SyntaxError: Expected token `)'"
-
-EquatorialPostition::EquatorialPostition()
-{
-
-}
